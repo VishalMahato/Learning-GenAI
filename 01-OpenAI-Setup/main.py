9f419b26105f9b3df1e8a1a3f647167301e0bdc1
VishalMahato <mailmevishalmahato@gmail.com>
2025-09-12 05:45:27 +0530

    openAi setup is done

--- 01-OpenAI-Setup/main.py
+++ 01-OpenAI-Setup/main.py
@@ -1,9 +1,18 @@
+import os
+from dotenv import load_dotenv
 from openai import OpenAI
+
+load_dotenv()
+# OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+OPENAI_MODEL = os.getenv("OPENAI_MODEL", "gpt-3.5-turbo")
 client = OpenAI()
 
-response = client.responses.create(
-    model="gpt-5",
-    input="Write a one-sentence bedtime story about a unicorn."
+
+response = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages =[
+        {"role": "user", "content" : "Hey There"}
+    ]
 )
 
-print(response.output_text)
+print(response.choices[0].message.content)
